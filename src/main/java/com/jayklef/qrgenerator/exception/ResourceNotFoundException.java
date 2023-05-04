062ccbe25261af12c32a645175be313cce2af74c
Jayklef <jerryarhawho@gmail.com>
2023-05-04 23:59:16 +0100

    Added ResourceNotFoundException

---
+++ src/main/java/com/jayklef/qrgenerator/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.jayklef.qrgenerator.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
