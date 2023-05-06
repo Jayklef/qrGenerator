000c927ef6bf8d51c36d15ec0809829ea2cbd68f
Jayklef <jerryarhawho@gmail.com>
2023-05-06 07:49:58 +0100

    Added ResourceNotFound to General Exception handler

--- src/main/java/com/jayklef/qrgenerator/exception/GeneralExceptionHandler.java
+++ src/main/java/com/jayklef/qrgenerator/exception/GeneralExceptionHandler.java
@@ -1,8 +1,26 @@
 package com.jayklef.qrgenerator.exception;
 
+import com.jayklef.qrgenerator.model.ErrorMessage;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ErrorMessage> ResourceNotFoundException(ResourceNotFoundException exception,
+                                                                  WebRequest webRequest){
+        ErrorMessage message = new ErrorMessage();
+        message.setStatusCode(HttpStatus.NOT_FOUND.value());
+        message.setMessage(exception.getMessage());
+        message.setTimestamp(new Date());
+
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
+    }
 }
