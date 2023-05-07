a1dde7e9fc3291839c418389f34d4da9da2f8716
Jayklef <jerryarhawho@gmail.com>
2023-05-07 23:48:30 +0100

    Handled Bad Request Error

--- src/main/java/com/jayklef/qrgenerator/exception/GeneralExceptionHandler.java
+++ src/main/java/com/jayklef/qrgenerator/exception/GeneralExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.jayklef.qrgenerator.exception;
 
 import com.jayklef.qrgenerator.model.ErrorMessage;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -10,6 +13,8 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
@@ -40,11 +45,21 @@ public class GeneralExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ErrorMessage> MethodArgumentMismatchException(MethodArgumentTypeMismatchException exception,
                                                                         WebRequest webRequest){
         ErrorMessage message = new ErrorMessage();
-        message.setStatusCode(HttpStatus.BAD_REQUEST.value());
+        message.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
         message.setMessage(exception.getMessage());
         message.setTimestamp(new Date());
 
-        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        Map<String, Object> body = new HashMap<>();
+        body.put("httpStatus", HttpStatus.BAD_REQUEST.value());
+        body.put("httpStatus", HttpStatus.BAD_REQUEST);
+        body.put("httpStatus", new Date());
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
 }
